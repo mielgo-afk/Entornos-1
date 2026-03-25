d0b6f3626cae82585bbab23c425192be6c4363c8
mielgo-admin <mielgo.uni@gmail.com>
2026-03-25 21:42:33 +0100

    no message

--- Assets/enemigo.cs
+++ Assets/enemigo.cs
@@ -3,16 +3,17 @@ using UnityEngine.AI;
 
 public class enemigo : MonoBehaviour
 {
-
     public Transform pointA;
     public Transform pointB;
 
     private NavMeshAgent agent;
     private Transform target;
+    private Animator animator;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
         target = pointA;
     }
 
@@ -20,10 +21,21 @@ public class enemigo : MonoBehaviour
     {
         agent.SetDestination(target.position);
 
+        
+        Vector3 velocity = agent.velocity;
+
+        
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+
+        
+        animator.SetFloat("X", localVelocity.x);
+        animator.SetFloat("Y", localVelocity.z);
+
         if (Vector3.Distance(transform.position, target.position) < 1f)
         {
             target = target == pointA ? pointB : pointA;
         }
     }
 }
+
 
